ff709165781185cdec5997c197a02cbfbcdb51c9
allenkc <aa87220@gmail.com>
2021-02-19 16:14:24 +0800

    [add] test data

---
+++ src/test_data/example.progress_spinner.ts
@@ -0,0 +1,13 @@
+export const testProgressSpinner = {
+    'id': '0',
+    'selector': 'progress-spinner-01',
+    'category': 'navigation',
+    'geometry': {
+        'width': 100,
+        'height': 100,
+        'x': 50,
+        'y': 50
+    },
+    'type': 'progress_spinner',
+    'name': 'test_spinner'
+}
